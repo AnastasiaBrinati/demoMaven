d48e3c2757e0871661a7c47af6a3a8e482a3393b
Anastasia <anastasia.brinati@students.uniroma2.eu>
2022-03-25 16:27:30 +0100

    Initial commit

---
+++ src/main/java/Libro.java
@@ -0,0 +1,12 @@
+public class Libro {
+    private String title;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
